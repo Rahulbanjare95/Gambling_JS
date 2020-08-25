0f4403124db36e88a185173594a9fd0775f48bdd
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-25 14:22:01 +0530

    Added: Caculated Total Amount won or loss after 20 days of play

--- gambling.js
+++ gambling.js
@@ -2,16 +2,20 @@ console.log("Welcome to Gambling Simulation");
 
 const betEveryGame = 1;
 let stakeEveryDay = 100;
+let day =1;
+var totalAmount = 0;
+let totalDays = 20;
 
 const LOOSER = 0;
 
 let limitPercentage = 50;
 let winingAmountToResign = ((stakeEveryDay * limitPercentage / 100 + stakeEveryDay));
 let loosingAmoutToResign = ((stakeEveryDay - stakeEveryDay * limitPercentage / 100));
-
+let limitValue = ((limitPercentage*stakeEveryDay/100));
 console.log("Maximum amount a player can win :" + winingAmountToResign);
 console.log("Minimum amount a player can be left with :" + loosingAmoutToResign);
 
+function stakeCalculator(){
 while (stakeEveryDay < winingAmountToResign && stakeEveryDay > loosingAmoutToResign) {
     let decider = Math.floor(Math.random() * 2);
     if (decider == LOOSER) {
@@ -21,4 +25,15 @@ while (stakeEveryDay < winingAmountToResign && stakeEveryDay > loosingAmoutToRes
         stakeEveryDay = ((stakeEveryDay - betEveryGame));
     }
 }
-console.log("Final amout today with player :: " + stakeEveryDay + "Rs");
+}
+
+for (let day = 0; day < totalDays; day++) {
+    stakeCalculator();
+    if (stakeEveryDay == winingAmountToResign) {
+        totalAmount = (totalAmount+limitValue);
+    }
+    else{
+        totalAmount = (totalAmount - limitValue);
+    }
+}
+console.log("Total amount won or loss == " + totalAmount);
