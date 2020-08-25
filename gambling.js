2c5d91007d69d79c74372ba0280b2486e0344469
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-25 12:00:10 +0530

    Added: function to decide the winner or looser by using inbuilt random function

--- gambling.js
+++ gambling.js
@@ -1,4 +1,20 @@
 console.log("Welcome to Gambling Simulation");
 
-const bet = 1;
-let stake = 100;
+const betEveryGame = 1;
+const stakeEveryDay = 100;
+
+
+const WINNER = 1;
+const LOOSER = 0;
+
+function winnerDecider() {
+
+    let decider = Math.floor(Math.random() * 2);
+    if (decider == 0) {
+        console.log ("Lost");
+    }
+    else
+        console.log("Won")    
+}
+
+winnerDecider();
