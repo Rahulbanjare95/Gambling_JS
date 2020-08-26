11526553a1a1ace229bfc8a3df18e13193544619
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-26 12:10:02 +0530

    Added: control to app using node

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Welcome To Gambling Simulator");
+const GamblingSimulator = require('./gambling');
+const gamblingSimulator = new GamblingSimulator();
+gamblingSimulator.simulateGame();
